37495f8540e425cfedc4f894eeb69df00751b6ee
ABHAY VISHWAKARMA <103497817+ABHAY-VISHWAKARMA-2629@users.noreply.github.com>
2022-04-12 16:13:30 +0530

    1st commit

---
+++ str/client/src/index.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+
+toast.configure();
+
+function App() {
+  const [product] = React.useState({
+    name: "Tesla Roadster",
+    price: 64998.67,
+    description: "Cool car"
+  });
+
+  async function handleToken(token, addresses) {
+    const response = await axios.post(
+      "http://localhost:5000/checkout",
+      { token, product }
+    );
+    const { status } = response.data;
+    console.log("Response:", response.data);
+    if (status === "success") {
+      toast("Success! Check email for details", { type: "success" });
+    } else {
+      toast("Something went wrong", { type: "error" });
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="product">
+        <h1>{product.name}</h1>
+        <h3>On Sale · ${product.price}</h3>
+      </div>
+      <StripeCheckout
+        stripeKey="pk_test_51KZwZ5SIBLdsYlql5WHC2n3N9yIEZ4N4W3yT18ExsiYBjC0IZVSsbWcTy08G2bkj41VHcJcfAinhqdl8L7BiPqZX00ADzkypa9"
+        token={handleToken}
+        amount={product.price * 100}
+        name="Tesla Roadster"
+        billingAddress
+        shippingAddress
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
